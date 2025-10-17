c21ff0c21e07c9199bc668de6a58ec4fd4281062
alexssokolov007-oss <alexssokolov007@gmail.com>
2025-10-17 13:04:47 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,20 +1,21 @@
-from src.power import power_function
-from src.constants import SAMPLE_CONSTANT
+from src.costants import OPERATIONS
+from calculator.calculator import calculate_expression
 
 
-def main() -> None:
+def main() -> float | str:
     """
-    Обязательнная составляющая программ, которые сдаются. Является точкой входа в приложение
-    :return: Данная функция ничего не возвращает
+    Основная функция: читает выражение, вычисляет результат и выводит его
+    Возвращает результат или 'Ошибка!' при исключении
     """
-
-    target, degree = map(int, input("Введите два числа разделенные пробелом: ").split(" "))
-
-    result = power_function(target=target, power=degree)
-
+    expression: str = str(input())
+    try:
+        result: float = calculate_expression(expression)
+    except Exception as e:
+        print(e)
+        return 'Ошибка!'
     print(result)
+    return result
 
-    print(SAMPLE_CONSTANT)
 
 if __name__ == "__main__":
     main()
