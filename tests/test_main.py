a7da877897c75a2c0ecc41feb87c29dfbec62ee7
alexssokolov007-oss <alexssokolov007@gmail.com>
2025-10-25 11:03:21 +0300

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,25 +2,25 @@ import src.main as main_module
 
 def test_main_good_expression():
     main_module.input = lambda: "2 + 3"
-    result = main_module.main()  # main() а не mm()
+    result = main_module.main()
     assert result == 5
 
 def test_main_divide_by_zero():
     main_module.input = lambda: "5 / 0" 
-    result = main_module.main()  # main() а не mm()
+    result = main_module.main()
     assert result == 'Ошибка!'
 
 def test_main_bad_expression():
     main_module.input = lambda: "2 + + 3"
-    result = main_module.main()  # main() а не mm()
+    result = main_module.main()
     assert result == 'Ошибка!'
 
 def test_main_one_number():
     main_module.input = lambda: "42"
-    result = main_module.main()  # main() а не mm()
+    result = main_module.main()
     assert result == 42
 
 def test_main_with_brackets():
     main_module.input = lambda: "(2 + 3) * 4"
-    result = main_module.main()  # main() а не mm()
+    result = main_module.main()
     assert result == 20
